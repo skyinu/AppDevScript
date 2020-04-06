417f8864355174c7150d7b5be7a2fc8221b5b846
skyinu <haochenu@gmail.com>
2020-04-06 22:49:04 +0800

    add arsc function

--- src/restools.js
+++ src/restools.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const childProcess = require('child_process');
 const APK_TOOL_PATH = path.join(__dirname, "..", "res", path.sep, "apktool_2.4.1.jar")
+const ARSC_BLAMER_PATH = path.join(__dirname, "..", "res", path.sep, "android-arscblamer.jar")
 let callApkTool = (command) => {
     let realComand = "java -jar " + APK_TOOL_PATH + command
     childProcess.exec(realComand, (error, stdout, stderr) => {
@@ -17,6 +18,23 @@ let callApkTool = (command) => {
     })
 }
 
+let callArscBlamer = (command) => {
+    let realComand = "java -jar " + ARSC_BLAMER_PATH + command
+    childProcess.exec(realComand, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`callArscBlamer exec error: ${error}`);
+            return;
+        }
+        if (stdout) {
+            console.log(`callArscBlamer stdout: ${stdout}`);
+        }
+        if (stderr) {
+            console.error(`callArscBlamer stderr: ${stderr}`);
+        }
+    })
+}
+
 module.exports = {
-    callApkTool
+    callApkTool,
+    callArscBlamer
 }
